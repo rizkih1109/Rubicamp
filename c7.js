4a9d39e5050f6a85b2c9d720eb9d4378617cd012
Rizki Hidayatulloh <rizkihidayatulloh@Rizkis-MacBook-Pro.local>
2023-07-31 20:05:08 +0700

    Tugas 7

---
+++ c7.js
@@ -0,0 +1,19 @@
+function weirdMultiple(num) {
+
+    if (num < 10) {
+        return num;
+    }
+
+    const A = num.toString().split('');
+    let hasil = 1;
+
+    for (let i = 0; i < A.length; i++) {
+        hasil *= A[i]
+    }
+    return weirdMultiple(hasil);
+
+} 
+
+console.log(weirdMultiple(39));
+console.log(weirdMultiple(999));
+console.log(weirdMultiple(3));
