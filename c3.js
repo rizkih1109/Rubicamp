86bb0703c8894baa00f75df65e64268a1e3616f4
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2023-08-09 15:33:15 +0700

    Tugas 3

--- c3.js
+++ c3.js
@@ -17,6 +17,7 @@ function romawi(n) {
             p = 'MDCXLVI';
         break;
     }
+    
     return p;
 }
 
@@ -24,7 +25,7 @@ console.log('Skrip Testing untuk Konversi Romawi/n')
 console.log('input | expected | result');
 console.log('______|__________|______');
 console.log('4     | IV       |', romawi(4));
-console.log('9     | IV       |', romawi(9));
-console.log('13    | IV       |', romawi(13));
-console.log('1453  | IV       |', romawi(1453));
-console.log('1646  | IV       |', romawi(1646));
+console.log('9     | IX       |', romawi(9));
+console.log('13    | XIII     |', romawi(13));
+console.log('1453  | MCDLIII  |', romawi(1453));
+console.log('1646  | MDCXLVI  |', romawi(1646));
