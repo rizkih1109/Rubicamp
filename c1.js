6214c0f3a3e950788244937d4e8f839d5eb56b2b
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2023-08-16 13:21:33 +0700

    Tugas 1

--- c1.js
+++ c1.js
@@ -1,4 +1,4 @@
-// Cara Pertama
+// // Cara Pertama
 function sum() {
     let total = 0;
     for (let i = 0; i < arguments.length; i++)
@@ -11,12 +11,23 @@ sum(11);
 sum(10,3,6,7,9);
 
 
-// Cara Kedua
+// // Cara Kedua
 function sum(...goods) {
     let total = 0;
     for (let good of goods)
         total += good;
     console.log(total);
+}
+
+sum(1,2,7);
+sum(1,4);
+sum(11);
+sum(10,3,6,7,9);
+
+
+// Cara Ketiga
+function sum() {
+    console.log(Object.values(arguments).reduce((total, num) => total + num));
 }
 
 sum(1,2,7);
