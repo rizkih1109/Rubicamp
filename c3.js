914a85994fdb2ceb57ec27f41f03274ca8c0c5ef
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2023-08-19 08:24:10 +0700

    Tugas 3

--- c3.js
+++ c3.js
@@ -1,3 +1,5 @@
+// // Cara Pertama
+
 function romawi(n) {
     let p = '';
     switch (n) {
@@ -19,6 +21,33 @@ function romawi(n) {
     }
     
     return p;
+}
+
+console.log('Skrip Testing untuk Konversi Romawi/n')
+console.log('input | expected | result');
+console.log('______|__________|______');
+console.log('4     | IV       |', romawi(4));
+console.log('9     | IX       |', romawi(9));
+console.log('13    | XIII     |', romawi(13));
+console.log('1453  | MCDLIII  |', romawi(1453));
+console.log('1646  | MDCXLVI  |', romawi(1646));
+
+
+// Cara Kedua
+function romawi(n) {
+    const library = {
+        M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90,
+        L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1
+    }
+
+    let hasil = '';
+    for (let x in library) {
+        while (n >= library[x]) {
+            n -= library[x]
+            hasil += x
+        }
+    }
+    return hasil;   
 }
 
 console.log('Skrip Testing untuk Konversi Romawi/n')
