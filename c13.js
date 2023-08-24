1d44bf79e03fd6e103daf495fa04be47b9eb0c5f
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2023-08-25 05:28:21 +0700

    Tugas 13

--- c13.js
+++ c13.js
@@ -4,19 +4,19 @@ const object = JSON.parse(move);
 const command = process.argv[2], id = process.argv[3]; submit = process.argv.slice(3).join(" ");
 let Oindex = id -1; count = object.length + 1;
 
-if (!command || command.toLowerCase() == 'help') {
+if (!command || command.toLowerCase() == 'help' || !command.toLowerCase() == 'help') {
     console.log(` >>> JS TODO <<<
-    $ node todo.js <command>
-    $ node todo.js list
-    $ node todo.js task <task_id>
-    $ node todo.js add <task_content>
-    $ node todo.js delete <task_id>
-    $ node todo.js complete <task_id>
-    $ node todo.js uncomplete <task_id>
-    $ node todo.js list:outstanding asc|desc
-    $ node todo.js list:complete asc|desc
-    $ node todo.js tag <tag_name_1> <tag_name_2> ... <tag_name_N>
-    $ node todo.js filter:<tag_name> 
+$ node todo.js <command>
+$ node todo.js list
+$ node todo.js task <task_id>
+$ node todo.js add <task_content>
+$ node todo.js delete <task_id>
+$ node todo.js complete <task_id>
+$ node todo.js uncomplete <task_id>
+$ node todo.js list:outstanding asc|desc
+$ node todo.js list:complete asc|desc
+$ node todo.js tag <tag_name_1> <tag_name_2> ... <tag_name_N>
+$ node todo.js filter:<tag_name> 
     `
     )
 } else {
@@ -32,8 +32,8 @@ if (!command || command.toLowerCase() == 'help') {
             break;
         case "add":
             if (submit) {
-                console.log(`${submit} telah ditambahkan`)
-                object.push({"ID": count, "title": submit, "complete": false, "tag": "" })
+                console.log(`"${submit}" telah ditambahkan`)
+                object.push({"ID": count, "title": submit, "complete": false, "tag": [] })
                 fs.writeFileSync('./Todo.json', JSON.stringify(object), 'utf-8')
             } else if (!submit || submit == " ") return
             break;
@@ -59,7 +59,7 @@ if (!command || command.toLowerCase() == 'help') {
             for (let i of object) {
                 if(!i.complete) {
                     i.complete = "[ ]"
-                    out.push(`${i.ID}: ${i.complete} ${i.title}.`)
+                    out.push(`${i.ID}. ${i.complete} ${i.title}.`)
                 }
             }
             if (id == "asc") console.log(out.join("\n"))
@@ -71,7 +71,7 @@ if (!command || command.toLowerCase() == 'help') {
             for (let i of object) {
                 if(i.complete) {
                     i.complete = "[x]"
-                    complete.push(`${i.ID}: ${i.complete} ${i.title}.`)
+                    complete.push(`${i.ID}. ${i.complete} ${i.title}.`)
                 }
             }
             if (id == "asc") console.log(complete.join("\n"))
@@ -79,7 +79,9 @@ if (!command || command.toLowerCase() == 'help') {
             break;
         case "tag":
             console.log(`Tag ${process.argv.slice(4)} telah ditambahkan ke dalam daftar '${object[object.findIndex(i => i.ID == id)].title}'`);
-            object[Oindex].tag = process.argv.slice(4);
+            process.argv.slice(4).forEach((item => {
+                if(!object[Oindex].tag.includes(item)) object[Oindex].tag.push(item)
+            }))
             fs.writeFileSync('./Todo.json', JSON.stringify(object), 'utf-8');
             break;
         case `filter:${command.slice(7)}`:
