58423880ac12cca71b18f321000642a8687b6d55
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2023-08-29 21:33:20 +0700

    Tugas 16

--- Tugas16/c16.js
+++ Tugas16/c16.js
@@ -12,16 +12,16 @@ class Car extends Tyre {
     constructor(brand, size, varian, sn, door, seat, warranty, year) {
         super(brand, size)
         this.varian = varian; this.sn = sn; this.door = door
-        this.seat = seat; this.warranty = warranty; this. year = year
+        this.seat = seat; this.warranty = warranty; this.year = year
     }
 
 }
 
-class Agya extends Car {}
+class Agya extends Car { }
 
-class Rush extends Car {}
+class Rush extends Car { }
 
-class CarFactory{
+class CarFactory {
     constructor() {
         this.cars = []
     }
@@ -76,13 +76,26 @@ class CarFactory{
     };
 
     static serialNumber() {
-        
-    };
-}
+        var d = new Date().getTime();
+        var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0;
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16;
+            if (d > 0) {
+                r = (d + r) % 16 | 0;
+                d = Math.floor(d / 16);
+            } else {
+                r = (d2 + r) % 16 | 0;
+                d2 = Math.floor(d2 / 16);
+            }
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+    }
+};
 
 const toyota = new CarFactory
 toyota.produce(2020)
 toyota.produce(2022)
 toyota.result()
 toyota.guaranteeSimulation(2025)
+
 
