da72ee116404ba0e2aab79b741d50505277c806d
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2024-01-01 14:49:14 +0700

    challenge 2 done

--- c2.js
+++ c2.js
@@ -3,22 +3,13 @@
 function deretKaskus(n) {
     const keltiga = [];
     for (let i = 0; i < n; i++) {
-    keltiga[i] = 3 * (i + 1);
-    if(keltiga[i] % 5 == 0 && keltiga[i] % 6 == 0) {
-        keltiga[i] = 'KASKUS';
-    } 
-    if(keltiga[i] % 6 == 0) {
-        keltiga[i] = 'KUS';
-    } 
-    if(keltiga[i] % 5 == 0) {
-        keltiga[i] = 'KAS';
+        keltiga[i] = 3 * (i + 1);
+        if (keltiga[i] % 5 == 0 && keltiga[i] % 6 == 0) keltiga[i] = 'KASKUS';
+        if (keltiga[i] % 6 == 0) keltiga[i] = 'KUS';
+        if (keltiga[i] % 5 == 0) keltiga[i] = 'KAS';
     }
-    
-    }
-
     return keltiga;
 }
-
 console.log(deretKaskus(10));
 
 
@@ -35,7 +26,6 @@ function deretKaskus(n) {
     }
     return kelbil;
 }
-
 console.log(deretKaskus(10))
 
 
